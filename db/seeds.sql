a65f4efb7257ec275afa27fabb66266f5dedbe1b
Derek Brown <derek.ethan.brown@gmail.com>
2017-11-01 18:26:30 -0400

    All good

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES (' Mumdo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Burger', false);
